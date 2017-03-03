e62588ecd36a858aacbebfdb49a8a4a831d0b618
mayanksatnalika <satnalikamayank12@gmail.com>
2017-03-03 22:03:26 +0530

    dfhdj

--- flask_serv.py
+++ flask_serv.py
@@ -95,5 +95,5 @@ def hello():
     
     return 'success'
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 80))
     app.run(host='0.0.0.0', port=port)
